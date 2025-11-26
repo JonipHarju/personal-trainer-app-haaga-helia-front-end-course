615686675460abbdefbbd1bfc42febaed810d7bc
jonipHarju <jonipharju@gmail.com>
2025-11-26 10:20:50 +0200

    feat: implement PageWrapper component and update App layout with it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import PageWrapper from "./components/PageWrapper";
 import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button variant={"default"} size={"lg"}>
-        Start training
-      </Button>
-    </div>
+    <PageWrapper>
+      <div className="flex mt-52 w-full flex-col items-center justify-center">
+        <Button variant={"default"} size={"lg"}>
+          Start training
+        </Button>
+      </div>
+    </PageWrapper>
   );
 }
 
