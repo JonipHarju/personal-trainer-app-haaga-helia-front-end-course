1c54ba4dd0ec9a44ac9e481c717c4ad8062e41c9
jonipHarju <jonipharju@gmail.com>
2025-12-01 08:28:56 +0200

    chore: install and setup TanStack query

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,9 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import FrontPage from "./components/FrontPage.tsx";
 import Customers from "./components/Customers.tsx";
 import Training from "./components/Training.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -21,6 +24,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
