9d669cddd1076ae712dc046de254b0f64db3f7ab
jonipHarju <jonipharju@gmail.com>
2025-12-05 17:27:13 +0200

    feat: define training columns and add a custom filtering function for the dates

---
+++ src/components/TrainingColumns.tsx
@@ -0,0 +1,33 @@
+import type { Training } from "@/interfaces/training";
+import type { ColumnDef } from "@tanstack/react-table";
+import { DateTime } from "luxon";
+
+export const trainingColumns: ColumnDef<Training>[] = [
+  {
+    accessorKey: "activity",
+    header: "Activity",
+  },
+  {
+    accessorKey: "date",
+    header: "Date",
+    cell: (cell) =>
+      DateTime.fromISO(cell.getValue() as string).toFormat("dd LLL yyyy HH:mm"),
+    filterFn: (row, columnId, filterValue) => {
+      if (!filterValue) return true;
+
+      const date = DateTime.fromISO(row.getValue(columnId) as string);
+      const { start, end } = filterValue;
+      if (start && date < DateTime.fromISO(start)) return false;
+      if (end && date > DateTime.fromISO(end).endOf("day")) return false;
+      return true;
+    },
+
+    enableColumnFilter: true,
+  },
+  {
+    accessorKey: "duration",
+    header: "Duration",
+    cell: (cell) => `${cell.getValue()} minutes`,
+    // TODO Fix filtering here
+  },
+];
