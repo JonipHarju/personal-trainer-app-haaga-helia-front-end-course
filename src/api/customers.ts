47b71cf0d24d096ff3422aff7eaea201880d9a88
jonipHarju <jonipharju@gmail.com>
2025-12-07 00:58:11 +0200

    feat: implement createCustomer function that sends a post request with the customerData to create a new customer.

--- src/api/customers.ts
+++ src/api/customers.ts
@@ -1,3 +1,5 @@
+import type { CustomerData } from "@/interfaces/customer";
+
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 export async function fetchAllCustomers() {
@@ -8,5 +10,19 @@ export async function fetchAllCustomers() {
     },
   });
   if (!response.ok) throw new Error("Network error");
+  return response.json();
+}
+
+export async function createCustomer(customerData: CustomerData) {
+  const response = await fetch(`${BASE_URL}/customers`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(customerData),
+  });
+
+  if (!response.ok) throw new Error("Customer creation faield ");
+
   return response.json();
 }
