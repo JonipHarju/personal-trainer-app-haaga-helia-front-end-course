7b8fb6ba1958302fdd386370179b67317bf96f1b
jonipHarju <jonipharju@gmail.com>
2025-12-09 01:33:59 +0200

    fix: change delete columns accessorKey to id in training columns to prevent the filter field appearing for that column

--- src/lib/trainingColumns.tsx
+++ src/lib/trainingColumns.tsx
@@ -58,7 +58,7 @@ export function getTrainingColumns(
       },
     },
     {
-      accessorKey: "delete",
+      id: "delete",
       header: "Delete",
       cell: ({ row }) => {
         const date = row.original;
